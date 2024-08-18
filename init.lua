3c39cb4bdeae9964c3d4be491c532f7afc90d068
Stoic <sme.nik.dov@gmail.com>
2024-08-18 09:34:09 +0500

    init

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("core/options")
+require("core/lazy")
+
+require("scripts/delete-buffer")
+require("scripts/terminal")
---
+++ test/test.vue
@@ -0,0 +1,95 @@
+<template>
+	<div style="margin-top: auto;" @click="handleClick">
+		<div class="footer app">
+			<div class="c-container">
+				<div class="logo-menu">
+					<div
+						class="logo"
+					>
+						<img
+							alt="Логотип ВДЕЛО"
+							src="../assets/images/logo-head-white.svg"
+						/>
+					</div>
+					<div
+						class="menu"
+						:style="{
+                        margin: $q.screen.width > mobileWidth ? '' : '0'
+                    }"
+					>
+						<template
+							v-if="$q.screen.width > mobileWidth"
+						>
+							<div class="menu__title">
+								Меню
+							</div>
+							<div class="menu__link">
+								<a href="/">Главная</a>
+							</div>
+							<div class="menu__link">
+								<a href="/investor">Инвесторам</a>
+							</div>
+							<div class="menu__link">
+								<a href="/loaner">Заёмщикам</a>
+							</div>
+							<div class="menu__link">
+								<a href="/partner">Партнёрам</a>
+							</div>
+						</template>
+						
+						<div class="menu__link">
+							<a href="/documents">Документы</a>
+						</div>
+					</div>
+				</div>
+				
+				<div
+					class="vdelo-info"
+				>
+					<div class="contacts">
+						<div class="phone">
+							<a
+								class="phone-link"
+								href="tel:+78002016618"
+							>8 800 201 66 18
+							</a>
+						</div>
+						<div class="email">
+							<a
+								class="mail-link"
+								href="mailto:support@vdelo.pro"
+							>support@vdelo.pro
+							</a>
+						</div>
+					</div>
+					<div class="all-rights-reserved">
+						© {{ new Date().getFullYear() }} ООО «ВДело». ОГРН 1206600030647. Все права защищены.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+    name:  'Footer',
+    props: {
+        isActiveAos: {
+            type:    Boolean,
+            default: false,
+        },
+    },
+    methods: {
+        handleClick() {
+            console.log(123);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.main {
+    background: red;
+}
+</style>
