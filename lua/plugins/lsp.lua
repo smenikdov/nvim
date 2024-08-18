5366aa0350d54c904a2360c7b5dba1425a6d5221
Stoic <sme.nik.dov@gmail.com>
2024-08-18 12:40:26 +0500

    Не получается сделать lsp для vue 2 ((

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,7 @@
+-- lcpconfig https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- volar install instruction https://github.com/vuejs/language-tools
+-- mason lspconfig https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#default-configuration
+
 return {
     {
         "williamboman/mason.nvim",
@@ -6,40 +10,112 @@ return {
     {
         "williamboman/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "lua_ls", "tsserver", "volar", "cssls", "html" },
+            ensure_installed = { "lua_ls", "tsserver", "volar", "vuels", "cssls", "html" },
         },
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({
-                -- init_options = {
-                -- 	plugins = {
-                -- 		{
-                -- 			name = "@vue/typescript-plugin",
-                -- 			location = "~/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin",
-                -- 			languages = { "vue" },
-                -- 		},
-                -- 	},
-                -- },
-                -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-            })
-
-            lspconfig.volar.setup({
-                init_options = {
-                    typescript = {
-                        tsdk =
-                        "/home/stoic/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib",
-                    },
-                    vue = {
-                        hybridMode = true,
-                    },
-                },
-            })
             lspconfig.cssls.setup({})
             lspconfig.html.setup({})
+            lspconfig.lua_ls.setup({})
+
+            if true then
+                if false then
+                    lspconfig.tsserver.setup({
+                        init_options = {
+                            plugins = {
+                                {
+                                    name = "@vue/typescript-plugin",
+                                    -- location = "~/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin",
+                                    location =
+                                    "~/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                                    languages = { "vue" },
+                                },
+                            },
+                        },
+                        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+                    })
+
+                    lspconfig.volar.setup({
+                        cmd = { "vue-language-server", "--stdio" },
+                        filetypes = { "vue" },
+                        init_options = {
+                            typescript = {
+                                tsdk =
+                                "/home/stoic/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib",
+                            },
+                            vue = {
+                                hybridMode = true,
+                            },
+                        },
+                    })
+                else
+                    lspconfig.volar.setup({
+                        cmd = { "vue-language-server", "--stdio" },
+                        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+                        init_options = {
+                            typescript = {
+                                tsdk =
+                                "/home/stoic/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib",
+                            },
+                            vue = {
+                                hybridMode = false,
+                            },
+                        },
+                    })
+                end
+            else
+                lspconfig.tsserver.setup({
+                    init_options = {
+                    },
+                    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+                })
+                lspconfig.vuels.setup({
+                    cmd = { "vls" },
+                    filetypes = { "vue" },
+                    init_options = {
+                        config = {
+                            css = {},
+                            emmet = {},
+                            html = {
+                                suggest = {},
+                            },
+                            javascript = {
+                                format = {},
+                            },
+                            stylusSupremacy = {},
+                            typescript = {
+                                format = {},
+                            },
+                            vetur = {
+                                completion = {
+                                    autoImport = false,
+                                    tagCasing = "kebab",
+                                    useScaffoldSnippets = false,
+                                },
+                                format = {
+                                    defaultFormatter = {
+                                        js = "none",
+                                        ts = "none",
+                                    },
+                                    defaultFormatterOptions = {},
+                                    scriptInitialIndent = false,
+                                    styleInitialIndent = false,
+                                },
+                                useWorkspaceDependencies = false,
+                                validation = {
+                                    script = true,
+                                    style = true,
+                                    template = true,
+                                },
+                            },
+                        },
+                    },
+                    -- root-dir
+                })
+            end
         end,
     },
 }
