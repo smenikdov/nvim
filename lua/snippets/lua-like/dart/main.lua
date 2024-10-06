32b82acf5ec59941c1d68d10ab4d184df3a5427b
stoic <sme.nik.dov@gmail.com>
2024-10-06 10:02:03 +0500

    -

--- lua/snippets/lua-like/dart/main.lua
+++ lua/snippets/lua-like/dart/main.lua
@@ -9,7 +9,7 @@ local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 local buffers_utils = require("scripts.utils.buffers")
 
-local code_place = "// code";
+local code_place = "// TODO";
 
 local snippets = {
     { "c", group = "Create" },
@@ -20,6 +20,54 @@ local snippets = {
             "if ({}) {{\n\t{}\n}}",
             { i(1, "true"), i(2, code_place)
         }),
+    },
+    {
+        name = "Create If Else",
+        trig = "cie",
+        body = fmt(
+            "if ({}) {{\n\t{}\n}} else {{\n\t{}\n}}",
+            { i(1, "true"), i(2, code_place), i(3, code_place)
+        }),
+    },
+    {
+        name = "Create If Else If",
+        trig = "ciei",
+        body = fmt(
+            "if ({}) {{\n\t{}\n}} else if ({}) {{\n\t{}\n}}",
+            { i(1, "true"), i(2, code_place), i(3, "true"), i(4, code_place)
+        }),
+    },
+    {
+        name = "Create Else",
+        trig = "ce",
+        body = fmt(
+            "else {{\n\t{}\n}}",
+            { i(1, code_place)
+        }),
+    },
+    {
+        name = "Create Else If",
+        trig = "cei",
+        body = fmt(
+            "else if ({}) {{\n\t{}\n}}",
+            { i(1, "true"), i(2, code_place)
+        }),
+    },
+    {
+        name = "Create For In",
+        trig = "cfi",
+        body = fmt(
+            "for (let {} in {}) {{\n\t{}\n}}",
+            { i(1, "key"), i(2, "object"), i(3, code_place) }
+        ),
+    },
+    {
+        name = "Create While",
+        trig = "cw",
+        body = fmt(
+            "while ({}) {{\n\t{}\n}}",
+            { i(1, "true"), i(2, code_place) }
+        ),
     },
 }
 
