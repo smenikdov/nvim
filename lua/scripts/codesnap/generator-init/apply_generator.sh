60826f449218991355a7bfa3ab0b669ff7153a34
Stoic <sme.nik.dov@gmail.com>
2025-02-04 12:23:59 +0500

    Bufferline auto close

--- lua/scripts/codesnap/generator-init/apply_generator.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source ./generator-init/check_os.sh
-
-function apply_generator {
-    cp -rf generator-init/"${os}generator.so" generator-init/generator.so
-}
-
-apply_generator $os
